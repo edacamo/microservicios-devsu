f1343c4acbd933f0c6cc1b299237e7ef592ec9c5
Adrian Carchipulla <edacamo@gmail.com>
2025-03-21 03:35:29 -0500

    Se agrega docker-compose del proyecto final y se ajusta el script de BD

--- DDL_PRUEBA_DEVSU.sql
+++ DDL_PRUEBA_DEVSU.sql
@@ -7,20 +7,20 @@ COLLATE utf8mb4_unicode_ci;
 CREATE TABLE persona (
 	 id SERIAL PRIMARY KEY,
 	 nombre VARCHAR(100) NOT NULL,
-	 genero VARCHAR(10),
+	 genero VARCHAR(1),
 	 edad INTEGER,
 	 identificacion VARCHAR(13) UNIQUE NOT NULL,
 	 direccion VARCHAR(255),
-	 telefono VARCHAR(20)
+	 telefono VARCHAR(20),
+	 CONSTRAINT ck_genero CHECK (genero IN ('M', 'F')),
 );
 
 /*Creacion tabla Cliente*/
 CREATE TABLE cliente (
-	 id SERIAL PRIMARY KEY,
 	 cliente_id VARCHAR(50) UNIQUE NOT NULL,
 	 contrasenia VARCHAR(255) NOT NULL,
 	 estado BOOLEAN,
-	 persona_id INTEGER,
+	 persona_id INTEGER PRIMARY KEY,
 	 CONSTRAINT fk_cliente_persona FOREIGN KEY (persona_id) REFERENCES persona (id)
 );
 
@@ -31,8 +31,8 @@ CREATE TABLE cuenta (
 	tipo_cuenta VARCHAR(50),
 	saldo_inicial NUMERIC,
 	estado BOOLEAN,
-	cliente_id INTEGER,
-	CONSTRAINT fk_cuenta_cliente FOREIGN KEY (cliente_id) REFERENCES cliente (id)
+	cliente_id VARCHAR,
+	CONSTRAINT fk_cuenta_cliente FOREIGN KEY (cliente_id) REFERENCES cliente (cliente_id)
 );
 
 /*Creacion tabla Movimientos*/
@@ -43,6 +43,7 @@ CREATE TABLE movimiento (
 	valor NUMERIC,
 	saldo NUMERIC,
 	cuenta_id INTEGER,
+	CONSTRAINT ck_tipo_movimiento CHECK (tipo_movimiento IN ('C', 'D')),
 	CONSTRAINT fk_movimiento_cuenta FOREIGN KEY (cuenta_id) REFERENCES cuenta (id)
 );
 
